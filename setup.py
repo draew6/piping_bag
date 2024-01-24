2839bb0ce381fa29ab6fdc058336ca7ff76af422
draew6 <adosko6@gmail.com>
2024-01-24 17:17:12 +0100

    added: setup and requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='piping_bag',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        "pydantic==2.5.3",
+        "asyncpg == 0.29.0",
+        "aiosqlite==0.19.0",
+    ],
+)
